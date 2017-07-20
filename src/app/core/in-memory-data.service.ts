8b2a948dce5bc7898264ac433b3d9fdc1e4dbc9e
masaki <masaki.iima@gmail.com>
2017-07-20 16:27:45 +0900

    feat:詳細画面入力フォーム作成

--- src/app/core/in-memory-data.service.ts
+++ src/app/core/in-memory-data.service.ts
@@ -4,7 +4,7 @@ export class InMemoryDataService implements InMemoryDbService {
   
   createDb() {
     let projects = [
-      { id: 1, title: '第１回' },
+      { id: 1, title: '第１回', date: '20170720', startTime: '1300', endTime: '1400', place: '会議室'},
       { id: 2, title: '第２回' },
       { id: 3, title: '第３回' },
       { id: 4, title: '第４回' },
